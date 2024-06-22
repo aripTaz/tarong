e03580c2adf9471fbf77d689b1272cd7cca3f03c
aripTaz <121712805+aripTaz@users.noreply.github.com>
2024-06-22 12:03:14 +0700

    Add files via upload

---
+++ tarong/MENU/m-system.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+CEKEXPIRED () {
+    today=$(date -d +1day +%Y-%m-%d)
+    Exp1=$(curl -sS https://raw.githubusercontent.com/aripTaz/ip/main/vps | grep $MYIP | awk '{print $3}')
+    if [[ $today < $Exp1 ]]; then
+    echo -e "\e[32mSTATUS SCRIPT AKTIF...\e[0m"
+    else
+    echo -e "\e[31mSCRIPT ANDA EXPIRED!\e[0m";
+
+    exit 0
+fi
+}
+IZIN=$(curl -sS https://raw.githubusercontent.com/aripTaz/ip/main/vps | awk '{print $4}' | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+echo -e "\e[32mPermission Accepted...\e[0m"
+CEKEXPIRED
+else
+echo -e "\e[31mPermission Denied!\e[0m";
+
+exit 0
+fi
+clear
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[0;100;33m          • SYSTEM MENU •          \E[0m"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+echo -e " [\e[36m•1\e[0m] MENU BOT"
+echo -e " [\e[36m•2\e[0m] MENU UPDATE"
+echo -e ""
+echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
+echo -e   ""
+echo -e   "Press x or [ Ctrl+C ] • To-Exit"
+echo -e   ""
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+read -p " Select menu : " opt
+echo -e ""
+case $opt in
+1) clear ; m-bot ; exit ;;
+2) clear ; menu-update ; exit ;;
+3) clear ; auto-reboot ; exit ;;
+4) clear ; restart ; exit ;;
+5) clear ; bw ; exit ;;
+6) clear ; m-tcp ; exit ;;
+7) clear ; webmin ; exit ;;
+0) clear ; menu ; exit ;;
+x) exit ;;
+*) echo -e "" ; echo "Anda salah tekan" ; sleep 1 ; m-system ;;
+esac
